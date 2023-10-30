32ffd63ddfdbc141d85cc6800b7bbebde95ce04a
Anıl Altunay <100069341+MentalistTR@users.noreply.github.com>
2023-10-30 19:30:42 +0300

    Update Project.sol

--- FinalProject/contracts/Project.sol
+++ FinalProject/contracts/Project.sol
@@ -1,6 +1,6 @@
  // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.19;
 
  contract Project {
 
@@ -32,7 +32,7 @@ pragma solidity ^0.8.7;
         address owner;
         string AssetName;
         bool active;
-        uint MonthPrice;  
+        uint256 MonthPrice;  
         HouseShop types;
      }
 
@@ -40,8 +40,8 @@ pragma solidity ^0.8.7;
         address owner;
         address leaser;
         string ProportName;
-        uint RentPrice;
-        uint RentStart;
+        uint256 RentPrice;
+        uint256 RentStart;
         uint256 RentEnd;
        
     }
@@ -49,7 +49,7 @@ pragma solidity ^0.8.7;
     struct complain {
         address leaser;
         address  owner;
-        uint HouseShopNumber;
+        uint16 HouseShopNumber;
         string reason;
         string decision;
         bool rightful;
@@ -59,21 +59,22 @@ pragma solidity ^0.8.7;
     struct endRentEarly {
         address owner;
         address leaser;
-        uint HouseShopNumber;
+        uint16 HouseShopNumber;
         bool leaserActive;
         bool ownerActive;
+        bool active;
     }
 
     event setHouseShop1(address indexed _owner,string _AssetName,string types1,uint _amount);
-    event getRent1(address indexed _leaser,address indexed _owner,uint256 _HouseShopNumber,string _ProportName,uint256 _RentEnd,uint _leaserAmount);
-    event endRet1(address indexed _owner,address indexed _leaser,uint256 _HouseShopNumber);
-    event removeHouseShopEvent(address indexed _owner,uint _houseShopNumber);
+    event getRent1(address indexed _leaser,address indexed _owner,uint16 _HouseShopNumber,string _ProportName,uint256 _RentEnd,uint256 _leaserAmount);
+    event endRet1(address indexed _owner,address indexed _leaser,uint16 _HouseShopNumber);
+    event removeHouseShopEvent(address indexed _owner,uint16 _houseShopNumber);
    
    function setHouseShop (
       address _owner,
       string memory _AssetName,
       string memory types1,
-      uint _amount) 
+      uint256 _amount) 
       external  Owner checkBlackList checkSender(_owner) {
 
       Property memory property;
@@ -99,7 +100,7 @@ pragma solidity ^0.8.7;
 
     }
  
-    function withdraw(uint _amount) payable external {
+    function withdraw(uint256 _amount) payable external {
         require(!locked, "Reentrancy call detected!");
         require(balances[msg.sender] >= _amount);
         balances[msg.sender] -= _amount;
@@ -112,7 +113,7 @@ pragma solidity ^0.8.7;
     function getRent (
        address payable _leaser,
        address  payable _owner,
-       uint256 _HouseShopNumber,
+       uint16 _HouseShopNumber,
        string memory _PropertyName,
        uint256 _RentEnd,
        uint256 _leaserAmount) 
@@ -123,11 +124,11 @@ pragma solidity ^0.8.7;
          require(checkRent(_owner,_HouseShopNumber) != true,"already rented");
          Property storage property = PropertyMap[ _owner][_HouseShopNumber];
          require(checkRentAmount(_owner,_HouseShopNumber) == _leaserAmount,"Invalid Price" );  
-        // require(keccak256(abi.encodePacked(property.AssetName)) == keccak256(abi.encodePacked(_PropertyName)),"Invalid House Name");
+       //  require(keccak256(abi.encodePacked(property.AssetName)) == keccak256(abi.encodePacked(_PropertyName)),"Invalid House Name");
          require(balances[msg.sender]>= _leaserAmount * (_RentEnd / 30),"insufficient funds");
          require(_RentEnd >=30 && _RentEnd % 30 == 0," multiple 30");
     
-          uint monthNumber = _RentEnd / 30;
+          uint256 monthNumber = _RentEnd / 30;
           require(monthNumber<=12," max 12 months.");         
           Contract memory _contracts;
           _contracts.leaser = _leaser;
@@ -153,7 +154,7 @@ pragma solidity ^0.8.7;
     function endRent(
     address _owner,
     address _leaser,
-    uint256 _HouseShopNumber) 
+    uint16 _HouseShopNumber) 
     external {
           Property storage property = PropertyMap[_owner][_HouseShopNumber];
           require(msg.sender == contracts[_owner][_HouseShopNumber].owner,"owner !=");
@@ -168,7 +169,7 @@ pragma solidity ^0.8.7;
             contracts[_owner].pop();
           } else {
 
-          for(uint i = _HouseShopNumber;i < contractMapLen -1;i++) {
+          for(uint16 i = _HouseShopNumber;i < contractMapLen -1;i++) {
             contracts[_owner][i] =  contracts[_owner][i +1];
 
           }
@@ -178,17 +179,17 @@ pragma solidity ^0.8.7;
           emit endRet1(_owner,_leaser,_HouseShopNumber);
     }
 
-    function removeHouseShop(address _owner,uint _houseShopNumber) external checkSender(_owner)  {
+    function removeHouseShop(address _owner,uint16 _houseShopNumber) external checkSender(_owner)  {
         require(PropertyMap[_owner].length > 0,"There is no asset");
         require(checkRent(_owner, _houseShopNumber) == false);
 
-       uint  propertyMapLen = PropertyMap[_owner].length;
+       uint propertyMapLen = PropertyMap[_owner].length;
 
        if( _houseShopNumber == propertyMapLen -1) {
             PropertyMap[_owner].pop();
        } else {
 
-       for(uint i = _houseShopNumber;i < propertyMapLen -1;i++) {
+       for(uint16 i = _houseShopNumber;i < propertyMapLen -1;i++) {
         PropertyMap[_owner][i] =  PropertyMap[_owner][i +1];
 
        }
@@ -198,7 +199,7 @@ pragma solidity ^0.8.7;
        emit removeHouseShopEvent( _owner,_houseShopNumber);
      }
 
-     function createComplain(address _leaser,address _owner,uint _HouseShopNumber, string memory _reason) public Owner {
+     function createComplain(address _leaser,address _owner,uint16 _HouseShopNumber, string memory _reason) public Owner {
            require(_leaser == checkContractLeaser(_owner,_HouseShopNumber),"you are not leaser");
            require(_owner == checkContractOwner(_owner,_HouseShopNumber),"Invalid owner address");
            require(checkTimeRent(_owner, _HouseShopNumber) > block.timestamp + 1296000 ,"15 days");
@@ -214,7 +215,7 @@ pragma solidity ^0.8.7;
               
     }
 
-    function decisionOwner(address _complainant,uint _compNumber,string memory _decision,bool _rightful) public {
+    function decisionOwner(address _complainant,uint16 _compNumber,string memory _decision,bool _rightful) public {
         require(msg.sender == owner,"You are not owner");
         require(complains[_complainant][_compNumber].active != true,"It is already decided.");
 
@@ -233,8 +234,7 @@ pragma solidity ^0.8.7;
         }  
     }
 
-
-    function endRentEarly1(address _leaser,address _owner,uint _contractMappingOwnerNumber,uint _HouseShopNumber) external Owner {
+    function endRentEarly1(address _leaser,address _owner,uint16 _contractMappingOwnerNumber,uint16 _HouseShopNumber) external Owner {
            require(checkContractLeaser(_owner,_contractMappingOwnerNumber) == _leaser,"You are not leaser");
            require(checkContractOwner(_owner,_contractMappingOwnerNumber) == _owner,"You are not owner");
            require(((checkTimeRent(_owner, _HouseShopNumber) - (block.timestamp)) >= 1296000),"15 days.");
@@ -243,83 +243,70 @@ pragma solidity ^0.8.7;
               _endRentEarly.leaser = _leaser;
               _endRentEarly.owner = _owner;
               _endRentEarly.HouseShopNumber = _HouseShopNumber;
+              if(msg.sender == _endRentEarly.leaser) { 
               _endRentEarly.leaserActive =true;
-              
-              if(msg.sender == _leaser) {
-                 EndRentEarly[_leaser].push(_endRentEarly);
               } else {
-                 EndRentEarly[_owner].push(_endRentEarly);
-             
-    }
-    }
-
-    function ownerResponseEarlyEnd(address _owner,address _leaser,uint _contractMappingOwnerNumber,uint _EndRentEarlyMappingNumber,bool _active) public Owner {
-            require(checkContractOwner(_owner,_contractMappingOwnerNumber) == _owner,"You are not owner");
-             endRentEarly storage _endRentEarly = EndRentEarly[_leaser][_EndRentEarlyMappingNumber];
-            _endRentEarly.ownerActive = _active;
+                _endRentEarly.ownerActive =true;
+              }
             
-             uint contractMapLen = contracts[_owner].length;
-            if(_endRentEarly.leaserActive == true && _endRentEarly.ownerActive == true) {
-                 
-            if(_endRentEarly.HouseShopNumber == contractMapLen -1) {
-            contracts[_owner].pop();
-            } else {
-
-          for(uint i = _endRentEarly.HouseShopNumber ;i < contractMapLen -1;i++) {
-            contracts[_owner][i] =  contracts[_owner][i +1];
-
-          }
-           contracts[_owner].pop();
-           }
-            Property storage property = PropertyMap[_owner][_endRentEarly.HouseShopNumber];
-            property.active = false;
-            }
+             EndRentEarly[_owner].push(_endRentEarly);
     }
 
-     function leaserResponseEarlyEnd(address _owner,address _leaser,uint _contractMappingOwnerNumber,uint _EndRentEarlyMappingNumber,bool _active) public Owner {
-            require(checkContractLeaser(_leaser,_contractMappingOwnerNumber) ==_leaser,"You are not owner");
+    function ownerLeaserResponseEarlyEnd(address _owner,uint16 _EndRentEarlyMappingNumber,bool _active) public Owner {
             endRentEarly storage _endRentEarly = EndRentEarly[_owner][_EndRentEarlyMappingNumber];
-            _endRentEarly.leaserActive = _active;
-
-           uint contractMapLen = contracts[_owner].length;
-            if(_endRentEarly.leaserActive == true && _endRentEarly.ownerActive == true) {
+            require(_endRentEarly.active == false,"Already responed!");
+            if(_endRentEarly.leaserActive == true) { 
+            require(_endRentEarly.owner == msg.sender ,"You are not leaser");
+            _endRentEarly.ownerActive = _active;
+            }
+            else if(_endRentEarly.ownerActive == true) {
+            require(_endRentEarly.leaser == msg.sender ,"You are not leaser");
+                _endRentEarly.leaserActive = _active;
+            }
+            else {
+                revert("You are not leaser or owner");
+            }
+            _endRentEarly.active = true;
+             uint contractMapLen = contracts[_owner].length;
+            if(_active == true) {
                  
             if(_endRentEarly.HouseShopNumber == contractMapLen -1) {
             contracts[_owner].pop();
             } else {
 
-          for(uint i = _endRentEarly.HouseShopNumber ;i < contractMapLen -1;i++) {
+           for(uint16 i = _endRentEarly.HouseShopNumber ;i < contractMapLen -1;i++) {
             contracts[_owner][i] =  contracts[_owner][i +1];
 
-          }
-           contracts[_owner].pop();
-          }
-            Property storage property = PropertyMap[_owner][_endRentEarly.HouseShopNumber];
-            property.active = false;
+            }
+            contracts[_owner].pop();
+            }
+              Property storage property = PropertyMap[_owner][_EndRentEarlyMappingNumber];
+              property.active = false;
+           
             }
     }
-  
-    function checkRent(address _owner,uint number) internal view returns (bool) {
+
+    function checkRent(address _owner,uint16 number) internal view returns (bool) {
         Property storage  property = PropertyMap[_owner][number];
                return property.active;
     }
 
-    function checkTimeRent(address _owner,uint number) internal view returns (uint) {
+    function checkTimeRent(address _owner,uint16 number) internal view returns (uint256) {
         Contract storage _contract = contracts[_owner][number];
         return _contract.RentEnd;
     }
 
-    function checkRentAmount(address _owner,uint _HouseShopnumber) internal  view returns (uint) {
+    function checkRentAmount(address _owner,uint16 _HouseShopnumber) internal  view returns (uint256) {
         Property storage property = PropertyMap[_owner][_HouseShopnumber];
         return property.MonthPrice;
     }
 
-    function checkContractLeaser(address _owner,uint number) internal view returns (address) {
+    function checkContractLeaser(address _owner,uint16 number) internal view returns (address) {
          Contract storage _contract = contracts[_owner][number];
          return _contract.leaser;
     }
 
-    function checkContractOwner(address _owner,uint number) internal view returns (address) {
+    function checkContractOwner(address _owner,uint16 number) internal view returns (address) {
          Contract storage _contract = contracts[_owner][number];
          return _contract.owner;
     }
